2db42d07cba1e599a05d392c46cbde2f2758ca1a
dcolliso@g.syr.edu <dcolliso@g.syr.edu>
2026-03-02 00:51:06 +0000

    1

---
+++ quickstart/target/run/my_new_project/models/example/my_second_dbt_model.sql
@@ -0,0 +1,18 @@
+
+  
+    
+
+create or replace transient table ANALYTICS.dbt_collisondatr.my_second_dbt_model
+    
+    
+    
+    as (-- Use the `ref` function to select from other models
+
+select *
+from ANALYTICS.dbt_collisondatr.my_first_dbt_model
+where id = 1
+    )
+;
+
+
+  
