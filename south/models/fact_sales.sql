049252c28fe77e0d1b68fed5229ac9878bc38911
Darrell Collison <dcolliso@g.syr.edu>
2026-03-27 21:16:55 +0000

    1

--- south/models/fact_sales.sql
+++ south/models/fact_sales.sql
@@ -1,25 +1,28 @@
-with stg_orders as 
-(
+with stg_orders as (
     select
         OrderID,  
         {{ dbt_utils.generate_surrogate_key(['employeeid']) }} as employeekey, 
         {{ dbt_utils.generate_surrogate_key(['customerid']) }} as customerkey, 
         replace(to_date(orderdate)::varchar,'-','')::int as orderdatekey
-    from {{source('northwind','Orders')}}
+    from {{ source('northwind','Orders') }}
 ),
-stg_order_details as
-(
+
+stg_order_details as (
     select 
         orderid,
         sum(Quantity) as quantityonorder, 
-        sum(Quantity*UnitPrice*(1-Discount)) as totalorderamount
-    from {{source('northwind','Order_Details')}}
-    group by orderid
-),
-
-stg_products as (
-    select * from {{ source('northwind','Products')}}
+        sum(Quantity * UnitPrice * (1 - Discount)) as totalorderamount
+    from {{ source('northwind','Order_Details') }}
+    group by 1
 )
-select  {{ dbt_utils.generate_surrogate_key(['stg_products.productid']) }} as productkey, 
-    stg_products.* 
-from stg_products
+
+select
+    o.OrderID,
+    o.employeekey,
+    o.customerkey,
+    o.orderdatekey,
+    od.quantityonorder,
+    od.totalorderamount
+from stg_orders o
+left join stg_order_details od 
+    on o.orderid = od.orderid
